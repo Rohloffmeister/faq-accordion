bf99e3ddc9f2120eeee02aff8190e48651b06ee3
rohloffmeister <sparkassenraeuber@gmail.com>
2025-02-24 13:36:44 +0100

    update

---
+++ script.js
@@ -0,0 +1,20 @@
+const faq_titles = document.querySelectorAll('.faq-title');
+
+/*
+faq_titles.forEach(title => {
+  title.addEventListener('click', () => {
+    console.log("test");
+    switch (title.getElementsByTagName('input')[0].checked) {
+        case true:
+            title.getElementsByTagName('input')[0].checked = false;
+            
+            break;
+        case false:
+            title.getElementsByTagName('input')[0].checked = true;
+            break;
+    
+        default:
+            break;
+    }
+})});
+*/
